ae203f92648e0485b9257c8e5eaa1b615d9a3bfe
aaudelin <aurelien.audelin@gmail.com>
2019-02-10 22:54:45 +0100

    Add exceptions

---
+++ src/main/java/exceptions/MissingArgumentException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class MissingArgumentException extends Exception {
+
+	public MissingArgumentException(String string) {
+		super(string);
+	}
+
+	private static final long serialVersionUID = 4410131683322786558L;
+
+}
